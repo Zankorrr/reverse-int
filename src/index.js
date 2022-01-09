3175484666bf8ad291e17b69dff59260da1cefbc
Zankorrr <95276425+Zankorrr@users.noreply.github.com>
2022-01-09 14:03:38 +0300

    feat: add reversing for integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let m = String(Math.abs(n));
+    let result = '';
+    for (let i = m.length; i > 0; i--) {
+        result = result + m[i - 1];
+    }
+    return result;
 }
